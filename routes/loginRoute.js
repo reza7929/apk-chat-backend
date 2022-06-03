cbd230fcc8e5743927d79ede195df3f31cca81ce
Reza Mohammadi <rezahd7929@gmail.com>
2022-06-03 22:47:53 +0430

    fixed error res

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -13,24 +13,19 @@ router.post("/", async (req, res) => {
 
     const userFound = await User.findOne({ userName });
     // check if user exist
-    if (!userFound)
-      return res.status(400).send({ error: "کاربر موردنظر یافت نشد" });
+    if (!userFound) return res.status(400).send("کاربر موردنظر یافت نشد");
     // check password
     if (!(await bcrypt.compare(password, userFound.password)))
-      return res.status(400).send({ error: "رمز عبور اشتباه است" });
+      return res.status(400).send("رمز عبور اشتباه است");
     // create the token
-    const token = jwt.sign(
-      { id: userFound.id, userName },
-      process.env.TOKEN_KEY,
-      {
-        expiresIn: "2h",
-      }
-    );
+    const token = jwt.sign({ userName }, process.env.TOKEN_KEY, {
+      expiresIn: "2h",
+    });
 
     return res.send(token);
   } catch (err) {
     console.log(err);
-    return res.status(500).send();
+    return res.status(500).send("خطای سرور");
   }
 });
 
